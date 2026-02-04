ae0939aa9cf6504ca7803d53b110125efde8054d
Pablo Yaguar <warspablo@gmail.com>
2026-02-04 17:56:53 -0500

    sendemail

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -1,24 +1,26 @@
 import nodemailer from 'nodemailer'
 
 const sendEmail = async ({ to, subject, html }) => {
+  const from = process.env.EMAIL_FROM || `"UEPMRMP" <${process.env.EMAIL_USER}>`
+
   const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
     port: Number(process.env.EMAIL_PORT) || 587,
     secure: false,
     auth: {
       user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS
-    }
+      pass: process.env.EMAIL_PASS,
+    },
   })
-
-  const from = process.env.EMAIL_FROM || `"Premium School" <${process.env.EMAIL_USER}>`
 
   await transporter.sendMail({
     from,
     to,
     subject,
-    html
+    html,
   })
+
+  console.log('Correo enviado a:', to, 'Asunto:', subject)
 }
 
 export default sendEmail
